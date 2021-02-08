3209f9f1013d16d04907ad5af64003eb73416607
asiankyle <46276920+asiankyle@users.noreply.github.com>
2021-02-08 15:10:34 -0800

    Update Watchlist.py

--- Watchlist.py
+++ Watchlist.py
@@ -2,14 +2,23 @@ import csv
 import sys
 #checks if the watchlist.csv file exists
 try:
-    w = open("Watchslist.csv")
+    w = open("Watchlist.csv")
 except IOError:
+    #this sys exit is temporary
     sys.exit("That file does not exist")
 
-
+Input_test = input("What ticker do you want to add?")
+print("Is this the ticker that you want to add? ",Input_test)
 
 #if it doesnt return an error message or create a csvfile
 
+#checks the watchlist for the Ticker
+def check_watchlist_for():
+    pass
+
+#adds the ticker to watchlist.csv
+def append_ticker_to_csv():
+    pass
 
 # reads the existing watchlist file
 with open('Watchlist.csv', newline = '') as csvfile:
