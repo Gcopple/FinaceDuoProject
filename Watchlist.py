74874c1e6d5deebb6ac1110bd8f998872c43730d
asiankyle <46276920+asiankyle@users.noreply.github.com>
2021-02-07 22:41:02 -0800

    Update Watchlist.py

--- Watchlist.py
+++ Watchlist.py
@@ -1,6 +1,11 @@
 import csv
 
+#checks if the watchlist.csv file exists
 
+#if it doesnt return an error message or create a csvfile
+
+
+# reads the existing watchlist file
 with open('Watchlist.csv', newline = '') as csvfile:
   spamreader = csv.reader(csvfile, delimiter = " ", quotechar = '|')
   for row in spamreader:
