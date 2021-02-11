f47132075670bb66ef53ed06210dc4ff3e8562bf
asiankyle <46276920+asiankyle@users.noreply.github.com>
2021-02-10 18:15:49 -0800

    Update check ticker for validity.py

--- check ticker for validity.py
+++ check ticker for validity.py
@@ -3,8 +3,29 @@ import csv
 import requests
 import sys
 
+list = open('Stocklist.csv')
+list_csv = csv.reader(list)
 
 
+requested_ticker = input('Please enter the ticker.')
+
+tickervalidity = False
+for row in list_csv:
+    if requested_ticker in row:
+        tickervalidity = True
+
+if tickervalidity == True:
+    print("That ticker is valid")
+else:
+    print(requested_ticker, ' is not valid.')
+
+
+
+#    print("123")
+#print("456")
+
+
+"""
 def get_listing():
     parameters = {}
     parameters["function"] = "LISTING_STATUS"
@@ -12,7 +33,7 @@ def get_listing():
     response = open(requests.get("https://www.alphavantage.co/query", params = parameters))
     csv_response = csv.reader(response)
     print(csv_response)
-
+"""
 #ticker = input("Please input desired ticker.")
 """
 with open(get_listing(), newline = "") as csvfile:
@@ -20,4 +41,3 @@ with open(get_listing(), newline = "") as csvfile:
     for row in spamreader:
         print(', '.join(row))
 """
-get_listing()
