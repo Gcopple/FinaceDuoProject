60bb6eaf9f471484fa64f173cd185291543f192b
asiankyle <46276920+asiankyle@users.noreply.github.com>
2021-02-22 16:24:50 -0800

    Update Watchlist.py

--- Watchlist.py
+++ Watchlist.py
@@ -3,11 +3,12 @@ import requests
 import sys
 #checks if the watchlist.csv file exists
 try:
-    w = open("Watchlist.csv")
+    w = open("Watchlist.txt")
 except IOError:
     #this sys exit is temporary
-    sys.exit("That file does not exist")
+    w = open("Watchlist.txt", 'x')
 
+#checks the ticker for realness
 def check_ticker_validity(requested_ticker):
     list = open('Stocklist.csv')
     list_csv = csv.reader(list)
@@ -17,21 +18,39 @@ def check_ticker_validity(requested_ticker):
             tickervalidity = True
     if tickervalidity == True:
         print("That ticker is valid and will be added")
+        return True
     else:
         print("debug: The ticker is nonexistent")
+        return False
+
+#checks the watchlist for the ticker
+#could possibly make a list instead of csv
+def check_watchlist_for(requested_ticker):
+    w_list = w.read()
+    watchlist_check = False
+    if requested_ticker in w_list:
+        watchlist_check = True
+    if watchlist_check == True:
+        print( requested_ticker, " is on the watchlist")
+        return True
+    else:
+        print( requested_ticker, " is not on the watchlist")
+        return False
+
+
 
 Input_test = input("What ticker do you want to add?")
 print("Is this the ticker that you want to add? ",Input_test)
-check_ticker_validity(Input_test)
+if check_ticker_validity(Input_test) == True:
+    if check_watchlist_for(Input_test) == False:
+        append_ticker_to_list(Input_test)
 #if it doesnt return an error message or create a csvfile
 
-#checks the watchlist for the Ticker
-def check_watchlist_for():
-    pass
+
 
 
 #adds the ticker to watchlist.csv
-def append_ticker_to_csv():
+def append_ticker_to_list(ticker):
     pass
 
 # reads the existing watchlist file
